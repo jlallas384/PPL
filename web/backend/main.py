c3f9585efd1937ad80937d4b5b573650b7f8d0ab
fuyura-dev <147980387+fuyura-dev@users.noreply.github.com>
2026-01-05 22:32:00 +0800

    add backend run API update frontend: - design - run button - display tokens in table

---
+++ web/backend/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+from compiler.lexer.lexer import Lexer, TokenKind
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+class RunRequest(BaseModel):
+    code: str
+
+
+@app.get("/")
+def check():
+    return {"status": "ok"}
+
+
+@app.post("/run")
+def run_code(req: RunRequest):
+    lexer = Lexer(req.code)
+    tokens = []
+
+    while True:
+        tok = lexer.lex()
+        if tok.kind == TokenKind.EOF:
+            break
+
+        tokens.append({
+            "kind": tok.kind.name,
+            "value": tok.value,
+            "line": tok.linenum,
+            "column": tok.column
+        })
+
+    return {"tokens": tokens}
--- web/frontend/src/App.jsx
+++ web/frontend/src/App.jsx
@@ -5,7 +5,9 @@ import Editor from "@monaco-editor/react";
 import { languageConf, languageDef, languageName } from "./langconfig";
 
 function App() {
-  const [code, setCode] = useState("// test");
+  const [code, setCode] = useState("// Type something here..");
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function handleEditorWillMount(monaco) {
     monaco.languages.register({
@@ -15,12 +17,35 @@ function App() {
     monaco.languages.setLanguageConfiguration(languageName, languageConf);
   }
 
+  async function run() {
+    setLoading(true);
+
+    const res = await fetch("http://localhost:8000/run", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ code }),
+    });
+    if (!res.ok) {
+      throw new Error(`Server error: ${res.status}`);
+    }
+    const data = await res.json();
+
+    const { tokens } = data;
+
+    setOutput(tokens);
+    setLoading(false);
+  }
+
   return (
     <div className="container">
       <div className="panel editor">
         <div className="header">
           <span>EDITOR</span>
-          <buttom className="run-btn">Run</buttom>
+          <button className="run-btn" onClick={run}>
+            Run
+          </button>
         </div>
         <div className="wrapper">
           <Editor
@@ -40,11 +65,28 @@ function App() {
           <span>OUTPUT</span>
         </div>
         <div className="wrapper">
-          <p>
-            asdfafasaaaaaaaaaaaaaaaaaaaaaaa1313123213aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadasdaaaaaaaaaaaaafsafs
-            fas asfdsa <br />
-            fasdf asdf asdf asf
-          </p>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            output && (
+              <table className="token-table">
+                <thead>
+                  <tr>
+                    <th>Kind</th>
+                    <th>Value</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {output.map((token, index) => (
+                    <tr key={index}>
+                      <td className="kind">{token.kind}</td>
+                      <td className="value">{token.value}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )
+          )}
         </div>
       </div>
     </div>
